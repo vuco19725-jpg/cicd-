f9888208e3620fe51034b559f77e74e411bc6313
vuco19725-jpg <vuco19725@gmail.com>
2026-05-24 10:17:08 +0800

    Fix server: add HTTP listener on port 8003

--- server/src/index.js
+++ server/src/index.js
@@ -1,2 +1,12 @@
-// CI/CD demo server entry point
-console.log("Hello from cicd-server");
+const http = require("http");
+
+const PORT = 8003;
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("Hello from cicd-server\n");
+});
+
+server.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
+});
