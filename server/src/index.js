65128b65c33ec769bca88b296440e03f63ce388b
vuco19725-jpg <vuco19725@gmail.com>
2026-05-24 13:53:06 +0800

    BREAK server for rollback test

--- server/src/index.js
+++ server/src/index.js
@@ -1,12 +1,1 @@
-const http = require("http");
-
-const PORT = 8003;
-
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("Hello from cicd-server\n");
-});
-
-server.listen(PORT, () => {
-  console.log("Server running on port " + PORT);
-});
+const BROKEN = ; // syntax error
